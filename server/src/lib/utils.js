9823ce2c0bb30cceb7978e7bd977335e87fa5f2c
Leo  Antelyes <lantelyes@quuantstamp.com>
2020-06-21 14:14:59 -0400

    fix possible move generation

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -9,7 +9,7 @@ const getAvailableMoves = (coordinates) => {
     [x + 1, y + 2],
     [x - 1, y + 2],
     [x + 2, y + 1],
-    [x - 2, y - 2],
+    [x - 1, y - 2],
     [x - 2, y + 1],
     [x - 2, y - 1],
     [x + 1, y - 2],
