9c96ce9c6122c07c7f341a2131e31f19529939d3
Leo  Antelyes <lantelyes@quuantstamp.com>
2020-06-21 16:42:24 -0400

    add timestamps and name to session schema

--- server/src/lib/db/models/session.js
+++ server/src/lib/db/models/session.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
-const sessionSchema = new mongoose.Schema({
-  moves: { type: [String], default: [] },
-});
+const sessionSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    moves: { type: [String], default: [] },
+  },
+  { timestamps: true },
+);
 
 const Session = mongoose.model('session', sessionSchema);
 
