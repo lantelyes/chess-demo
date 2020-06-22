57917252b34a80bb747a936864c58f68fcc100d2
Leo  Antelyes <lantelyes@quuantstamp.com>
2020-06-21 22:57:26 -0400

    add no sessions found message

--- client/src/components/LoadSessionModal.js
+++ client/src/components/LoadSessionModal.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
+import { isEmpty } from 'lodash';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import { Modal, Button, Table } from 'react-bootstrap';
 import { useSession } from '../contexts/Session';
+
+const NoSessionsFoundMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+`;
 
 const LoadSessionModal = ({ show, onClose }) => {
   const [selectedSessionId, setSelectedSessionId] = useState(null);
@@ -14,33 +23,37 @@ const LoadSessionModal = ({ show, onClose }) => {
       <Modal.Header closeButton>
         <Modal.Title>Load Session</Modal.Title>
       </Modal.Header>
-
-      <Table striped bordered>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Moves</th>
-            <th>Last Move</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sessions.map((session, i) => (
-            <tr
-              key={`load-session-${i + 1}`}
-              onClick={() => setSelectedSessionId(session._id)}
-              style={{
-                backgroundColor:
-                  selectedSessionId === session._id ? '#c9c9c9' : 'unset',
-              }}
-            >
-              <td>{session.name}</td>
-              <td>{session.moves.length}</td>
-              <td>{session.moves.slice(-1)[0]}</td>
+      {!isEmpty(sessions) ? (
+        <Table striped bordered>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Moves</th>
+              <th>Last Move</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-
+          </thead>
+          <tbody>
+            {sessions.map((session, i) => (
+              <tr
+                key={`load-session-${i + 1}`}
+                onClick={() => setSelectedSessionId(session._id)}
+                style={{
+                  backgroundColor:
+                    selectedSessionId === session._id ? '#c9c9c9' : 'unset',
+                }}
+              >
+                <td>{session.name}</td>
+                <td>{session.moves.length}</td>
+                <td>
+                  {session.moves.length ? session.moves.slice(-1)[0] : 'None'}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <NoSessionsFoundMessage>No Sessions Found</NoSessionsFoundMessage>
+      )}
       <Modal.Footer>
         <Button
           variant="secondary"
