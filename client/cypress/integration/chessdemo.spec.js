924ea2321798e0113011009f05c86527ae0caa86
Leo  Antelyes <lantelyes@quuantstamp.com>
2020-06-23 21:48:50 -0400

    clean up test

--- client/cypress/integration/chessdemo.spec.js
+++ client/cypress/integration/chessdemo.spec.js
@@ -7,12 +7,12 @@ describe('Main Page', () => {
     cy.get('.react-joyride__spotlight').should('be.visible');
   });
 
-  it('Shows the initial tutorial tooltop', () => {
+  it('Shows the initial tutorial tooltip', () => {
     cy.get('.react-joyride__tooltip').should('be.visible');
   });
 
   it('Should skip the the initial tutorial, and hide it', () => {
-    cy.get('[aria-label="Close"] > svg').click();
+    cy.get('[style="align-items: center; display: flex; justify-content: flex-end; margin-top: 15px;"] > div > button').click(); //Selector for skip button
     cy.get('.react-joyride__spotlight').should('not.be.visible');
     cy.get('.react-joyride__tooltip').should('not.be.visible');
   });
